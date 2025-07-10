8babacea955cfc8bd01c33c611db01babcbfbf27
Flor <mariaflorenciaperezd@gmail.com>
2025-07-10 21:37:22 +0200

    feat: book-list component setted whit the bonus and a toggle

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-list.component.html'
 })
 export class BookListComponent {
-  constructor() { }
-} 
+
+  books = this.bookService.books;
+  statusFilter: 'all' | 'read' | 'unread' = 'all';
+
+  constructor(private bookService: BookService) {}
+
+  toggleStatus(id: string) {
+    this.bookService.toggleStatus(id);
+  }
+  
+  filteredBooks() {
+    const list = this.books();
+    if (this.statusFilter === 'all') return list;
+    return list.filter(book => book.status === this.statusFilter);
+  }
+}
