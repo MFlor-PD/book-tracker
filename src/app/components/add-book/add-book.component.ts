158e20ce55ce116288396e414bc1e16eaaf45d25
Flor <mariaflorenciaperezd@gmail.com>
2025-07-10 20:46:50 +0200

    feat: add-book created with Reacctive Forms and commonModule

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BookService} from '../../services/book.service';
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-book.component.html'
 })
 export class AddBookComponent {
-  constructor() { }
-} 
+  bookForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private bookService: BookService) {
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      status: ['unread']
+    });
+  }
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+      const newBook = {
+        id: Date.now().toString(),
+        ...this.bookForm.value
+      };
+      
+      this.bookService.addBook(newBook);
+      this.bookForm.reset({status: 'unread'});
+    }
+  }
+}
